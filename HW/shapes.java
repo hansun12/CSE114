803214864b6a10aa3c72ec7cce9d7647d73ff2f9
hansun12 <songhs-0601@naver.com>
2023-03-11 14:33:01 +0900

    :memo:init

---
+++ HW/shapes.java
@@ -0,0 +1,18 @@
+package HW;
+
+public class shapes {
+    public static void main (String[] args) {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("         *");
+        System.out.println("***********");
+        System.out.println("*         **");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
+    }
+
+}
---
+++ Simple.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+public class Simple {
+
+    public static void main (String[] args) {
+        
+        System.out.println("Welcome to CS!!!");
+        System.out.println("Let's have some fun.");
+
+    }
+
+}
+
+
