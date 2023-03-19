41f11b18934212f5fe2229fcde1a3b190e71ad49
hansun song <songhs-0601@naver.com>
2023-03-20 03:22:36 +0900

    test

--- testgit/tryinggit.java
+++ testgit/tryinggit.java
@@ -3,5 +3,6 @@ package CSE114.testgit;
 public class tryinggit {
     public static void main(String[] args){
         System.out.println("hello");
+        System.out.println("it will work this time");
     }
 }
