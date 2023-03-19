b30c2b23d7de2eae9442b0d39154a2b3ad4baa8b
hansun12 <songhs-0601@naver.com>
2023-03-20 03:15:49 +0900

    test

--- check/check.java
+++ check/check.java
@@ -3,5 +3,6 @@ package CSE114.check;
 public class check {
     public static void main(String[] args){
         System.out.println("sad");
+        System.out.println("hopefuly not more");
     }
 }
