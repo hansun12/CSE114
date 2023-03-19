98e06f93edd71a0032adebd3a06db41b042c966e
hansun12 <songhs-0601@naver.com>
2023-03-20 03:16:42 +0900

    removed file

--- check/check.java
+++
@@ -1,8 +0,0 @@
-package CSE114.check;
-
-public class check {
-    public static void main(String[] args){
-        System.out.println("sad");
-        System.out.println("hopefuly not more");
-    }
-}
