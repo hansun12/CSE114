af76c4ea7354c52a11fd4fecf32aa370d7bb8429
hansun12 <songhs-0601@naver.com>
2023-03-20 03:23:18 +0900

    deleted things

--- testgit/tryinggit.java
+++
@@ -1,8 +0,0 @@
-package CSE114.testgit;
-
-public class tryinggit {
-    public static void main(String[] args){
-        System.out.println("hello");
-        System.out.println("it will work this time");
-    }
-}
