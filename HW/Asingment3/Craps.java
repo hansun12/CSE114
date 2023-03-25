791704382e5204bc39b7fade752d8be3315c348d
hansun12 <songhs-0601@naver.com>
2023-03-25 16:15:21 +0900

    HW3

---
+++ HW/Asingment3/Craps.java
@@ -0,0 +1,17 @@
+package CSE114.HW.Asingment3;
+
+public class Craps {
+
+    public static int evaluateRoll(int )
+
+    public static void roll(){
+
+    }
+
+    public static void point(){
+
+    }
+    public static void main(String[] args){
+        point()
+    }
+}
