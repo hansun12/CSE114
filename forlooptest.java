e209de500004307059a270e2db326b9646f4c972
hansun12 <songhs-0601@naver.com>
2023-03-30 04:16:05 +0900

    classwork

---
+++ forlooptest.java
@@ -0,0 +1,13 @@
+package CSE114;
+
+public class forlooptest {
+    public static void forLoop(int num) {
+        for(int i = 0; i <= num; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        forLoop(5);
+    }
+}
