748d204e386866ffa6cb59e7ac06e8a1854b06f5
hansun12 <songhs-0601@naver.com>
2023-04-03 15:34:00 +0900

    HW

---
+++ ttest.java
@@ -0,0 +1,16 @@
+package CSE114;
+
+public class ttest {
+    public static int test(){
+        return ;
+    }
+    public static void main(String[] args) {
+        int a = test(40);
+        System.out.println(a);
+        String b = "F";
+        if(b.equals("F")){
+            a = 20;
+        }
+        System.out.println(a);
+    }
+}
