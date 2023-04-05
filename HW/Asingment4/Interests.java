30714f4c0bbb001fa75a3263e0530bc21c473a2b
hansun12 <songhs-0601@naver.com>
2023-04-05 21:28:08 +0900

    HW

--- HW/Asingment4/Interests.java
+++ HW/Asingment4/Interests.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Interests {
 
+    public static int Calculate()
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
